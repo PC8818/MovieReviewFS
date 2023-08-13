e400c09e4260f70fe57832ce1c0190ccb0709bee
Pranav <94358279+PC8818@users.noreply.github.com>
2023-08-13 19:40:47 +0530

    Update movie.js

--- Frontend/movie.js
+++ Frontend/movie.js
@@ -2,7 +2,7 @@ const url = new URL(location.href);
 const movieId = url.searchParams.get("id")
 const movieTitle = url.searchParams.get("title")
 
-const APILINK='https://backend-proj.pranavchand.repl.co/api/v1/reviews/';
+const APILINK='https://backend-proj.pranavchand.repl.co/api/v1/reviews/'; //different for all
   
 
 const main = document.getElementById("section");
